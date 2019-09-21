cdd165fbfbf61b5ca0ddabc1028c5e9328c78c5c
evvfebruary <FireBulletred14Yn>
2019-09-21 17:10:47 +0300

    # Add content types

--- handlers/http_metadata.py
+++ handlers/http_metadata.py
@@ -1,3 +1,15 @@
 ACCEPTED_METHODS = ('GET', 'HEAD')
 SERVER = 'Vovingx prefork server'
 HTTP_VERSION = 'HTTP/1.1'
+
+CONTENT_TYPES = {
+    'js': 'application/javascript',
+    'gif': 'image/gif',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'swf': 'application/x-shockwave-flash',
+    'txt': 'text/plain',
+    'css': 'text/css',
+    'html': 'text/html'
+}
