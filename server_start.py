4ac6ba8b048813d9db4609158297336a4990e8b0
evvfebruary <FireBulletred14Yn>
2019-09-23 01:33:34 +0300

    # Config path fixed

--- server_start.py
+++ server_start.py
@@ -3,5 +3,5 @@ from handlers.config import read_config
 
 
 if __name__ == '__main__':
-    server = Server(read_config("/Users/evv/PycharmProjects/vovingx/config/httpd.conf"))
+    server = Server(read_config("/etc/httpd.conf"))
     server.server_init()
