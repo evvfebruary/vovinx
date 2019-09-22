c9adcb855f7da256230845563ef4afc830457edb
evvfebruary <FireBulletred14Yn>
2019-09-23 01:28:19 +0300

    # Useless validation

--- handlers/http_handlers.py
+++ handlers/http_handlers.py
@@ -3,21 +3,18 @@ from collections import namedtuple
 from urllib.parse import unquote
 from handlers.logger import logger
 
-Request = namedtuple("Request", 'body method validated path')
+Request = namedtuple("Request", 'body method path')
 
 
 def request_handler(input_request):
     method, path = None, None
     request_body = input_request.decode('utf-8')
     request_by_string = request_body.split(" ")
-    validate = True if len(request_by_string) >= 3 else False  # Minimum length ( head example )
-    if validate:
-        method = request_by_string[0]
-        path = unquote(request_by_string[1].split('?')[0])
+    method = request_by_string[0]
+    path = unquote(request_by_string[1].split('?')[0])
 
     processing_request = Request(input_request.decode('utf-8'),
                                  method,
-                                 validate,
                                  path)
     return processing_request
 
