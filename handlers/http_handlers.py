ad16db1664f520e70f0faa620dd80f58d5012a1a
evvfebruary <FireBulletred14Yn>
2019-09-22 23:20:04 +0300

    # Request handler added

---
+++ handlers/http_handlers.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+from urllib.parse import unquote
+from handlers.logger import logger
+
+Request = namedtuple("Request", 'body method validated path')
+
+
+def request_handler(input_request):
+    method, path = None, None
+    logger.info(f"Input raw request {input_request} \n")
+    request_body = input_request.decode('utf-8')
+    request_by_string = request_body.split(" ")
+    validate = True if len(request_by_string) >= 3 else False # Minimum length ( head example )
+    if validate:
+        print("ff")
+        method = request_by_string[0]
+        path = unquote(request_by_string[1].split('?')[0])
+
+    processing_request = Request(input_request.decode('utf-8'),
+                                 method,
+                                 validate,
+                                 path)
+    return processing_request
