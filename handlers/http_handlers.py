6612950ee891ee20d2fa4ea1561c33639342b17d
evvfebruary <FireBulletred14Yn>
2019-09-23 01:18:50 +0300

    # Response handler

--- handlers/http_handlers.py
+++ handlers/http_handlers.py
@@ -1,3 +1,4 @@
+import datetime
 from collections import namedtuple
 from urllib.parse import unquote
 from handlers.logger import logger
@@ -20,3 +21,20 @@ def request_handler(input_request):
                                  validate,
                                  path)
     return processing_request
+
+
+def response_handler(http_version=None, status_code=None,
+                     server=None, content_len=None, content_type=None, data=None):
+    no_content_response = "\r\n".join([f"{http_version} {int(status_code)} {status_code.name}",
+                 f"Server: {server}",
+                 f"Date: {datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"])
+    response_string = no_content_response
+    if content_len is not None:
+        response_string += f"Content-Length: {content_len}\r\n"
+    if content_type is not None:
+        response_string += f"Content-Type: {content_type}\r\n"
+    response_string += '\r\n'
+    response_string = response_string.encode('utf-8')
+    if data is not None:
+        response_string += data
+    return response_string
